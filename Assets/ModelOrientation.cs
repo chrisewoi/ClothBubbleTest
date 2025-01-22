83a7a359dd9e6358b0939eca981864a26db1f650
chrisewoi <69186935+chrisewoi@users.noreply.github.com>
2025-01-22 18:13:23 +1100

    tweek

--- Assets/ModelOrientation.cs
+++ Assets/ModelOrientation.cs
@@ -27,12 +27,13 @@ public class ModelOrientation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float currentSmoothTime = smoothTime;
         modelRot = gameObject.transform.rotation.eulerAngles;
 
         if (Input.GetAxisRaw("Horizontal") > 0)
         {
             targetRot = -90f;
-            idleTime = Time.time;            
+            idleTime = Time.time;   
         } 
         else if (Input.GetAxisRaw("Horizontal") < 0)
         {
@@ -43,8 +44,9 @@ public class ModelOrientation : MonoBehaviour
         {
             targetRot = 0;
             randomIdle = Random.Range(0f, maxRandIdle);
+            currentSmoothTime = smoothTime / Random.Range(0.1f, 3f);
         }
-        transform.rotation = Quaternion.Euler(modelRot.x, Mathf.SmoothDampAngle(modelRot.y, targetRot + offset, ref v, smoothTime), modelRot.z);
+        transform.rotation = Quaternion.Euler(modelRot.x, Mathf.SmoothDampAngle(modelRot.y, targetRot + offset, ref v, currentSmoothTime), modelRot.z);
 
     }
 }
